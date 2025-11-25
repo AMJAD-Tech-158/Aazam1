2ce16c07d5f8ae29eb4aeeea21852a17925e331e
Amjad-Tech-158 <amjadalitech158@gmail.com>
2025-11-25 20:55:00 +0530

    first commit

---
+++ index.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    int a,b;
+    printf("Enter 1st Number : \n");
+    scanf("%d",&a);
+    printf("Enter 2nd No : \n");
+    scanf("%d",&b);
+    int sum = a+b;
+    printf("Result is : %d",sum);
+
+    return 0;
+}
